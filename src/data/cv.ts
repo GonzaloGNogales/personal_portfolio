32ba12f86ddba569a8987d642c5b5cd3d1d96d15
GonzaloGNogales <gonzalognbusiness@gmail.com>
2025-11-17 20:49:37 +0100

    Bug resolve for pdfs

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -51,7 +51,9 @@ export const skills = [
 ];
 
 import RCID3DCAteaser from '@/assets/RCID3DCAteaser.jpg';
+import RCI3DCApaper from '@/assets/RCID3DCApaper.pdf'
 import UTMCCSteaser from '@/assets/UTMCCSteaser.jpg';
+import UTMCCSpaper from '@/assets/UTMCCSpaper.pdf'
 
 export const publications = [
 	{
@@ -62,7 +64,7 @@ export const publications = [
 		link: 'https://doi.org/10.1145/3687266',
 		abstract: 'We propose a novel contact-aware method to synthesize highly-dense 3D crowds of animated characters. Existing methods animate crowds by, first, computing the 2D global motion approximating subjects as 2D particles and, then, introducing individual character motions without considering their surroundings. This creates the illusion of a 3D crowd, but, with density, characters frequently intersect each other since character-to-character contact is not modeled. We tackle this issue and propose a general method that considers any crowd animation and resolves existing residual collisions. To this end, we take a physics-based approach to model contacts between articulated characters. This enables the real-time synthesis of 3D high-density crowds with dozens of individuals that do not intersect each other, producing an unprecedented level of physical correctness in animations. Under the hood, we model each individual using a parametric human body incorporating a set of 3D proxies to approximate their volume. We then build a large system of articulated rigid bodies, and use an efficient physics-based approach to solve for individual body poses that do not collide with each other while maintaining the overall motion of the crowd. We first validate our approach objectively and quantitatively. We then explore relations between physical correctness and perceived realism based on an extensive user study that evaluates the relevance of solving contacts in dense crowds. Results demonstrate that our approach outperforms existing methods for crowd animation in terms of geometric accuracy and overall realism.',
 		teaser: RCID3DCAteaser,
-		pdfLink: 'assets/RCID3DCApaper.pdf',
+		pdfLink: RCI3DCApaper,
 		videoLink: 'https://www.youtube.com/watch?v=gP8FFeKRqx0&ab_channel=DanCasas',
 		webLink: 'https://mslab.es/projects/Dense3DCrowds/'
 	},
@@ -74,7 +76,7 @@ export const publications = [
 		link: 'https://arxiv.org/abs/2510.10256',
 		abstract: 'We propose a novel contact-aware method to synthesize highly-dense 3D crowds of animated characters. Existing methods animate crowds by, first, computing the 2D global motion approximating subjects as 2D particles and, then, introducing individual character motions without considering their surroundings. This creates the illusion of a 3D crowd, but, with density, characters frequently intersect each other since character-to-character contact is not modeled. We tackle this issue and propose a general method that considers any crowd animation and resolves existing residual collisions. To this end, we take a physics-based approach to model contacts between articulated characters. This enables the real-time synthesis of 3D high-density crowds with dozens of individuals that do not intersect each other, producing an unprecedented level of physical correctness in animations. Under the hood, we model each individual using a parametric human body incorporating a set of 3D proxies to approximate their volume. We then build a large system of articulated rigid bodies, and use an efficient physics-based approach to solve for individual body poses that do not collide with each other while maintaining the overall motion of the crowd. We first validate our approach objectively and quantitatively. We then explore relations between physical correctness and perceived realism based on an extensive user study that evaluates the relevance of solving contacts in dense crowds. Results demonstrate that our approach outperforms existing methods for crowd animation in terms of geometric accuracy and overall realism.',
 		teaser: UTMCCSteaser,
-		pdfLink: 'assets/UTMCCSpaper.pdf',
+		pdfLink: UTMCCSpaper,
 		videoLink: '',
 		webLink: ''
 	},
